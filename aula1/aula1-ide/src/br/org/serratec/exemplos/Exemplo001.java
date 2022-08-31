8143672e9769b36f27d4c7ea05aee85cdd282ce9
Nathan Dias <nathanfaria9@gmail.com>
2022-08-31 13:45:42 -0300

    Ajustes Pastas final

---
+++ aula1/aula1-ide/src/br/org/serratec/exemplos/Exemplo001.java
@@ -0,0 +1,11 @@
+package br.org.serratec.exemplos;
+
+public class Exemplo001 {
+		
+		public static void main(String[] args){
+				
+			System.out.println("Nathan");
+			System.out.println("Dias");
+		}
+		//CamelCase
+}
---
+++ aula1/aula1-ide/src/br/org/serratec/exemplos/Exemplo003.java
@@ -0,0 +1,19 @@
+package br.org.serratec.exemplos;
+
+public class Exemplo003 {
+
+	public static void main(String[] args) {
+		//CNTRL + ALT SETA BAIXO CIMA REPLICA LN
+		System.out.println("Programa em Java - Aula-1");
+		
+		/*
+		System.out.println("Programa em Java - Aula-2");
+		System.out.println("Programa em Java - Aula-3");
+		System.out.println("Programa em Java - Aula-4");
+		System.out.println("Programa em Java - Aula-5");
+		*/
+		
+
+	}
+
+}
