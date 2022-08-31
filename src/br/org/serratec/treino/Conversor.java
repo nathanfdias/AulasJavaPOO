dc2646f6bb9e69e002833cf968303d0ecd2f851c
Nathan Dias <nathanfaria9@gmail.com>
2022-08-31 15:29:29 -0300

    Adicionando Treino

--- src/br/org/serratec/treino/Conversor.java
+++ src/br/org/serratec/treino/Conversor.java
@@ -1,7 +1,18 @@
 package br.org.serratec.treino;
 
-public class Conversor {
+import java.util.Scanner;
+
+public class Conversor { //Conversor Celsius Fahrenheit
     public static void main(String[] args) {
         
+      // A try-with-resources statement.
+        try (Scanner sc = new Scanner(System.in)) {
+            // Asking the user to input a temperature in Celsius and then converting it to Fahrenheit.
+            System.out.println("Digite a temperatura em Celsius:");
+            Double numCelsius = sc.nextDouble();
+            // Converting the temperature from Celsius to Fahrenheit.
+            Double  numFahrenh = (numCelsius * 1.8) + 32;
+            System.out.println("Temperatura em Fahrenheit: "+ numFahrenh + "º");
+        }             
     }
 }
